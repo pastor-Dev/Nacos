7b548248e0317ec9bd13934e4136142f16804164
pastor-Dev <datapwapurity01@gmail.com>
2025-11-01 10:35:35 +0100

    update on the voting platform

--- Voting/views.py
+++ Voting/views.py
@@ -26,7 +26,7 @@ def get_client_ip(request):
 
 
 @login_required
-def election_list(request):
+def election_List(request):
     """Display list of all elections"""
     elections = Election.objects.all()
     
